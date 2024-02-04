e935b8b70c736b173bfd4df555610b350c60c642
kiselevivan320 <kiselevivan320@gmail.com>
2024-02-04 18:28:14 +0600

    add hw10

--- terraform/stage/storage-bucket.tf
+++ terraform/stage/storage-bucket.tf
@@ -6,8 +6,8 @@ terraform {
     bucket     = "ivankiselev320"
     region     = "ru-central1"
     key        = "terraform/terraform.tfstate"
-    access_key = "access_key"
-    secret_key = "secret_key"
+    access_key = "YCAJEUE4mBEtlYLIIu-T-Yg_v"
+    secret_key = "YCMnqeYEreuIjUmwgYffP44B9VOVhpfsa9SNBnc1"
 
     skip_region_validation      = true
     skip_credentials_validation = true
